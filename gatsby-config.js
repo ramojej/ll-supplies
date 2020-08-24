af1bc99f90baeecb44857d01964f708e36231fd6
Jejo <jejomardorongon@gmail.com>
2020-08-24 09:23:06 +0800

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-176065048-1`,
+      },
+    },
+    {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
         /*
