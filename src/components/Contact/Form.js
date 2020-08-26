9a830d2f0f259595e7bb36b78fd4ba31ba6cd0c7
Jejo <jejomardorongon@gmail.com>
2020-08-26 18:21:37 +0800

    Used react-netlify-forms

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react"
+import {
+  useNetlifyForm,
+  NetlifyFormProvider,
+  NetlifyFormComponent,
+  Honeypot,
+} from "react-netlify-forms"
 import { useForm } from "react-hook-form"
 import tw, { styled } from "twin.macro"
 
@@ -73,15 +79,18 @@ const FormContainer = styled.div`
 //   colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
 // }
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
-}
-
 const Form = () => {
   const { register, errors, handleSubmit, formState } = useForm({
     mode: "onChange",
+  })
+
+  const netlify = useNetlifyForm({
+    name: "react-hook-form",
+    action: "/thanks",
+    honeypotName: "bot-field",
+    onSuccess: (response, context) => {
+      console.log("Successfully sent form data to Netlify Server")
+    },
   })
 
   const { isSubmitting } = formState
@@ -90,23 +99,8 @@ const Form = () => {
 
   //const [party, setParty] = useState(false)
 
-  const onSubmit = (values, e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        values,
-      }),
-    })
-      .then(() => clearVisibleForm(false))
-      .catch(error => alert(error))
-    //console.log(values)
+  const onSubmit = data => netlify.handleSubmit(null, data)
 
-    //setParty(true)
-  }
   return (
     <FormContainer>
       {!visibleForm && (
@@ -116,79 +110,84 @@ const Form = () => {
         </>
       )}
       {visibleForm && (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <input type="hidden" name="form-name" value="contact" />
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="ENTER YOUR NAME"
-              className={errors.name ? "error" : ""}
-              ref={register({
-                required: "Name is required",
-                minLength: {
-                  value: 2,
-                  message: "Name must be atleast 2 characters",
-                },
-                maxLength: {
-                  value: 100,
-                  message: "Name should not be more than 100 characters",
-                },
-              })}
-            />
-            {errors.name && errors.name.message && <p>{errors.name.message}</p>}
-          </div>
-          <div>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="ENTER YOUR EMAIL"
-              className={errors.email ? "error" : ""}
-              ref={register({
-                required: "Email is required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Invalid email address.",
-                },
-              })}
-            />
-            {errors.email && errors.email.message && (
-              <p>{errors.email.message}</p>
-            )}
-          </div>
-          <div>
-            <input
-              type="text"
-              id="phone"
-              name="phone"
-              placeholder="PHONE NUMBER"
-              className={errors.phone ? "error" : ""}
-              ref={register({
-                required: "Phone number is required",
-                pattern: {
-                  value: /\+65(6|8|9)\d{7}/g,
-                  message: "Invalid phone number.",
-                },
-              })}
-            />
-            {errors.phone && errors.phone.message && (
-              <p>{errors.phone.message}</p>
-            )}
-          </div>
-          <div>
-            <textarea
-              id="message"
-              name="message"
-              placeholder="ENTER YOUR MESSAGE"
-              ref={register}
-            />
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? "Submitting..." : "Submit"}
-          </button>
-        </form>
+        <NetlifyFormProvider>
+          <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
+            <Honeypot />
+            <div>
+              <input type="hidden" name="form-name" value="contact" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="ENTER YOUR NAME"
+                className={errors.name ? "error" : ""}
+                ref={register({
+                  required: "Name is required",
+                  minLength: {
+                    value: 2,
+                    message: "Name must be atleast 2 characters",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message: "Name should not be more than 100 characters",
+                  },
+                })}
+              />
+              {errors.name && errors.name.message && (
+                <p>{errors.name.message}</p>
+              )}
+            </div>
+            <div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="ENTER YOUR EMAIL"
+                className={errors.email ? "error" : ""}
+                ref={register({
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Invalid email address.",
+                  },
+                })}
+              />
+              {errors.email && errors.email.message && (
+                <p>{errors.email.message}</p>
+              )}
+            </div>
+            <div>
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="PHONE NUMBER"
+                className={errors.phone ? "error" : ""}
+                ref={register({
+                  required: "Phone number is required",
+                  pattern: {
+                    value: /\+65(6|8|9)\d{7}/g,
+                    message: "Invalid phone number.",
+                  },
+                })}
+              />
+              {errors.phone && errors.phone.message && (
+                <p>{errors.phone.message}</p>
+              )}
+            </div>
+            <div>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="ENTER YOUR MESSAGE"
+                ref={register}
+              />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </button>
+          </NetlifyFormComponent>
+        </NetlifyFormProvider>
       )}
     </FormContainer>
   )
