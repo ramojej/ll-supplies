bef9db19adef73875342ea4bdf89c359d17bf5e3
Jejo <jejomardorongon@gmail.com>
2020-09-09 17:07:55 +0800

    Changed logo prop

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,8 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-const Logo = color => {
-  console.log(color)
+const Logo = ({ color }) => {
+  //console.log(color)
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "logo-red-a.png" }) {
